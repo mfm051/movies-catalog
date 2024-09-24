dec232bdbf4d10f16b8a33873b92c4e5d098d372
Mateus Matos <98349389+mfm051@users.noreply.github.com>
2024-09-24 13:54:44 -0300

    Adiciona model Country

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_09_24_165009) do
+ActiveRecord::Schema[7.1].define(version: 2024_09_24_165309) do
+  create_table "countries", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "genres", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", null: false
