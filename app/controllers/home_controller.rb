e367845d8827cc6031b0a108f1f373c53b874d83
Mateus Matos <98349389+mfm051@users.noreply.github.com>
2024-09-25 19:57:16 -0300

    Adiciona seção para filmes em rascunho

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,6 +2,7 @@ class HomeController < ApplicationController
   def index
     @genres = Genre.all
     @directors = Director.all
-    @movies = Movie.all
+    @movies_published = Movie.where(published: true)
+    @movies_draft = Movie.where(published: false)
   end
 end
