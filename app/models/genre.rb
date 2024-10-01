0eb3b7c0d6c347e40837c8bebfb969369be81072
Mateus Matos <98349389+mfm051@users.noreply.github.com>
2024-10-01 14:00:50 -0300

    Aprimora validação de nome para Genre

--- app/models/genre.rb
+++ app/models/genre.rb
@@ -2,5 +2,13 @@ class Genre < ApplicationRecord
   has_many :movies, dependent: :destroy
   has_many :directors, dependent: :destroy
 
-  validates :name, presence: true
+  validates :name, presence: true, uniqueness: true
+
+  before_validation :standardize_name
+
+  private
+
+  def standardize_name
+    self.name = name.titleize
+  end
 end
