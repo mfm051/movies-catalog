adcd403ee5d6599d0f3603afd8709aff58289529
Mateus Matos <98349389+mfm051@users.noreply.github.com>
2024-10-01 14:30:41 -0300

    Aprimora validação de nome para Director

--- app/models/director.rb
+++ app/models/director.rb
@@ -2,7 +2,14 @@ class Director < ApplicationRecord
   belongs_to :country
   belongs_to :genre
 
-  validates :name, presence: true
+  validates :name, presence: true, uniqueness: true
+  before_validation :standardize_name
 
   has_many :movies, dependent: :destroy
+
+  private
+
+  def standardize_name
+    self.name = name.titleize
+  end
 end
